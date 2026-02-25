547ea6b5a2bf29fec48c01d6c02c28812f8b657f
hank.milliken94@protonmail.com <Hank Milliken>
2026-02-25 02:44:32 +0100

    delete unnecessary storage code

--- apps/five-in-a-row/src/localStorage/storageKeys.ts
+++
@@ -1,7 +0,0 @@
-export const STORAGE_KEYS = {
-    THEME_MODE: 'five-in-a-row.theme-mode',
-    BOARD_SIZE: 'five-in-a-row.board-size',
-    GAME_MODE: 'five-in-a-row.game-mode',
-    OPENING_PROTOCOL: 'five-in-a-row.opening-protocol',
-    BOT_LEVEL: 'five-in-a-row.bot-level',
-} as const;
