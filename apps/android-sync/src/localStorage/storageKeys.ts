4ec069738d6bc3b7a76ee5970d688eee00eaed5e
hank.milliken94@protonmail.com <Hank Milliken>
2026-02-25 02:44:32 +0100

    delete unnecessary storage code

--- apps/android-sync/src/localStorage/storageKeys.ts
+++
@@ -1,5 +0,0 @@
-export const STORAGE_KEYS = {
-    THEME_MODE: 'android-sync.theme-mode',
-    DEBUG_MODE: 'android-sync.debug-mode',
-    EVOLU_MNEMONIC: 'android-sync.evolu-mnemonic',
-} as const;
