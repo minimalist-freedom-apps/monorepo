9d8edc0826bc745bf3027974ec866ca9281cc216
hank.milliken94@protonmail.com <Hank Milliken>
2026-03-01 17:26:02 +0100

    fix restore seed duplication

--- apps/price-converter/src/app/SettingsScreen/SettingsScreen.tsx
+++ apps/price-converter/src/app/SettingsScreen/SettingsScreen.tsx
@@ -1,15 +1,14 @@
 import type { DebugSettingsDep } from '@minimalist-apps/fragment-debug';
-import type { BackupMnemonicDep, RestoreMnemonicServiceDep } from '@minimalist-apps/fragment-evolu';
+import type { BackupMnemonicDep, RestoreMnemonicDep } from '@minimalist-apps/fragment-evolu';
 import { SettingsScreen } from '@minimalist-apps/fragment-settings';
 import type { ThemeModeSettingsDep } from '@minimalist-apps/fragment-theme';
 import type { GoBackDep } from '@minimalist-apps/navigator';
 import type { FC } from 'react';
-import { RestoreSeed } from './RestoreSeed';
 
 type SettingsScreenDeps = ThemeModeSettingsDep &
     DebugSettingsDep &
     BackupMnemonicDep &
-    RestoreMnemonicServiceDep &
+    RestoreMnemonicDep &
     GoBackDep;
 
 export type SettingsScreenDep = { SettingsScreen: FC };
@@ -18,7 +17,7 @@ export const SettingsScreenPure = (deps: SettingsScreenDeps) => (
     <SettingsScreen goBack={deps.goBack}>
         <deps.ThemeModeSettings />
         <deps.BackupMnemonic />
-        <RestoreSeed restoreMnemonic={deps.restoreMnemonic} />
+        <deps.RestoreMnemonic />
         <deps.DebugSettings />
     </SettingsScreen>
 );
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
         setupFiles: ['./vitest.setup.ts'],
+        exclude: [...configDefaults.exclude, '**/e2e/**', '**/*.e2e.test.ts', '**/*.e2e.ts'],
     },
 });
