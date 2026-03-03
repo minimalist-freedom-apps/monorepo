1a5c7e9ea6e5a4926fef5f2a0f23b1cdbe6dbefb
hank.milliken94@protonmail.com <Hank Milliken>
2026-03-03 21:12:47 +0100

    fix unittest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
+import { fileURLToPath } from 'node:url';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: ['./vitest.setup.ts'],
+        setupFiles: [fileURLToPath(new URL('./vitest.setup.ts', import.meta.url))],
         exclude: [...configDefaults.exclude, '**/e2e/**', '**/*.e2e.test.ts', '**/*.e2e.ts'],
     },
 });
